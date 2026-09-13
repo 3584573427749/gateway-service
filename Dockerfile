d8943cc03348d2ef0cddc460b8d40971ef9535e9
Kjell <kjell@kejpa.com>
2026-09-13 11:39:50 +0300

    Tagit bort filer, beroenden och städat onödigt inför utvecklingsstart

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM php:8.5-fpm-alpine
 
-RUN apk add --no-cache bash git curl mariadb-client oniguruma-dev \
-    && docker-php-ext-install pdo pdo_mysql
+RUN apk add --no-cache bash git curl
 
 COPY --from=composer:2 /usr/bin/composer /usr/bin/composer
 
@@ -16,7 +15,7 @@ COPY . .
 COPY docker/*.sh /
 RUN chmod +x /*.sh
 
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["entrypoint.sh"]
 
 CMD ["php", "-S", "0.0.0.0:8080", "-t", "public"]
 EXPOSE 8080
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 set -e
 
-#echo "[INIT] Waiting for DB..."
-#
-#until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-#  sleep 1
-#done
+echo "Starting gw-service..."
 
-#echo "[INIT] DB ready"
-
-echo "[INIT] Running migrations..."
-/migrate.sh
-
-echo "[INIT] Dumping tables..."
-/dump_tables.sh
-
-echo "[INIT] Starting app..."
+if [ ! -f .env ]; then
+    echo "No .env file found."
+    echo "Copying .env.example -> .env"
+    cp .env.example .env
+else
+    echo ".env already exists."
+fi
 
 exec "$@"
